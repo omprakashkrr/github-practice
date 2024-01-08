bfd0c81bb00abc58be63a9d0430e21ac0504e0c9
Om Prakash <omprakash.krr@gmail.com>
2024-01-08 14:53:45 +0530

    add new keyword in console myclass

--- OopsPractice/Constructor-Chaning/MyClass.cs
+++ OopsPractice/Constructor-Chaning/MyClass.cs
@@ -20,7 +20,7 @@ namespace OopsPractice.Constructor_Chaning
 
         public void DisplayMyValue()
         {
-            Console.WriteLine($"My Value is:{MyValue}");
+            Console.WriteLine($"My New Value is:{MyValue}");
         }
     }
 
